67318197061f0932410219bc05a0df99d0de747a
Forrest Steele <fsteele2025@chadwickschool.org>
2025-03-14 10:30:17 -0700

    beginning Wordle game additions

---
+++ GameWindow.java
@@ -0,0 +1,40 @@
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+/**
+ * Provides methods to construct and manipulate the main game window. 
+ *
+ * @author Forrest Steele
+ * @version March 2025
+ */
+public class GameWindow
+{
+    private String targetWord;
+    private String guessWord;
+    
+    public GameWindow() {
+        
+    }
+    
+    public void run() {
+        
+    }
+    
+    public void actionPerformed(ActionEvent event) {
+        
+    }
+    
+}
